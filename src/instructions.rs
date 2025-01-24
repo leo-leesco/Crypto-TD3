7286cbfae4bb3979240e6fa7d027419fedd52193
Léo LEESCO <leo.leesco@gmail.com>
2025-01-24 12:15:34 +0100

    start quarterround

---
+++ src/instructions.rs
@@ -0,0 +1,3 @@
+use crate::ARRAY_SIZE;
+
+fn quarter_round(state: [u32; ARRAY_SIZE], i: usize, j: usize, k: usize, l: usize) {}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+#![allow(non_snake_case)]
+
+const ARRAY_SIZE: usize = 16;
+
+mod instructions;
